f87096bcfe4088addc9692734eff2c8dfcc14b83
Daniil Zhodik <daniilzhodik628@gmail.com>
2021-12-08 02:04:54 +0300

    added Class Main

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Formula formula=new Formula();
+        formula.setVisible(true);
+    }
+}
